946b98ec950153bb6a957a7602810257c3b62ec0
Mohammad ali moshaee <power2craft@gmail.com>
2020-09-22 23:06:13 +0330

    test

--- resources/views/app/layouts/master.blade.php
+++ resources/views/app/layouts/master.blade.php
@@ -7,7 +7,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-    <title>tatabogh.com masha 22</title>
+    <title>tatabogh.com ali rahmani</title>
 
     <meta name="keywords" content="" />
     <meta name="description" content="">
